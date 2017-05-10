32e2355b3210ef8d20242fcbb4b1b84798d60918
Jody Schofield <jodyscho@gmail.com>
2017-05-10 18:10:44 -0400

    Add an Rx sample to the templates

--- vip-templates/VIP+Rx.xctemplate/___FILEBASENAME___Interactor.swift
+++ vip-templates/VIP+Rx.xctemplate/___FILEBASENAME___Interactor.swift
@@ -1,13 +1,20 @@
 import RxSwift
 
 class ___FILEBASENAMEASIDENTIFIER___Interactor {
-    
+
+    // Todo: Replace...
+    fileprivate let eventModel = PublishSubject<___FILEBASENAMEASIDENTIFIER___InteractorResponse>()
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___Interactor: ___FILEBASENAMEASIDENTIFIER___InteractorInput {
     
+    func fetchModel() {
+        eventModel.onNext(___FILEBASENAMEASIDENTIFIER___InteractorResponse())
+    }
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___Interactor: ___FILEBASENAMEASIDENTIFIER___InteractorOutput {
-
+    var model: Observable<___FILEBASENAMEASIDENTIFIER___InteractorResponse> {
+        return eventModel as Observable<___FILEBASENAMEASIDENTIFIER___InteractorResponse>
+    }
 }
