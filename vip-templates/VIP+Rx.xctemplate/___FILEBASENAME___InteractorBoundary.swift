99056f1ad9b5295b0c6927d1fd5615c657e16ffe
Mathew Cartmill <mathewcartmill@Mathews-MacBook-Pro.local>
2017-05-10 16:58:49 -0400

    Rename inputoutput to boundary

---
+++ vip-templates/VIP+Rx.xctemplate/___FILEBASENAME___InteractorBoundary.swift
@@ -0,0 +1,11 @@
+struct ___FILEBASENAMEASIDENTIFIER___InteractorResponse {
+    
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___InteractorInput {
+    
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___InteractorOutput {
+    
+}
