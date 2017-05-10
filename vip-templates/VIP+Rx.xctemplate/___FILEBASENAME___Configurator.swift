57bdb1216cea705119d7bf9505cf08fe35559f0e
Mathew Cartmill <mathewcartmill@Mathews-MacBook-Pro.local>
2017-05-10 18:29:46 -0400

    Remove Default Interactor

--- vip-templates/VIP+Rx.xctemplate/___FILEBASENAME___Configurator.swift
+++ vip-templates/VIP+Rx.xctemplate/___FILEBASENAME___Configurator.swift
@@ -3,7 +3,7 @@ final class ___FILEBASENAMEASIDENTIFIER___Configurator {
     func configure(_ viewController: ___FILEBASENAMEASIDENTIFIER___ViewController) {
 
         let router = ___FILEBASENAMEASIDENTIFIER___Router(viewController)
-        let interactor = Default___FILEBASENAMEASIDENTIFIER___Interactor()
+        let interactor = ___FILEBASENAMEASIDENTIFIER___Interactor()
         let presenter = ___FILEBASENAMEASIDENTIFIER___Presenter(interactor)
 
         viewController.interactor = interactor
