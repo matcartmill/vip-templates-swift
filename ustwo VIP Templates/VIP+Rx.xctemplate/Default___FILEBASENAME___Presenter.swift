d151ad146ae5674531d3120825818bd8dd71c590
Mathew Cartmill <mathewcartmill@Mathews-MacBook-Pro.local>
2017-05-10 13:13:29 -0400

    Add VIP+Rx Template

---
+++ ustwo VIP Templates/VIP+Rx.xctemplate/Default___FILEBASENAME___Presenter.swift
@@ -0,0 +1,16 @@
+class Default___FILEBASENAMEASIDENTIFIER___Presenter {
+
+    weak var output: ___FILEBASENAMEASIDENTIFIER___PresenterOutput?
+    
+    init(output: ___FILEBASENAMEASIDENTIFIER___PresenterOutput) {
+        self.output = output
+    }
+}
+
+extension Default___FILEBASENAMEASIDENTIFIER___Presenter: ___FILEBASENAMEASIDENTIFIER___Presenter {
+    
+}
+
+extension Default___FILEBASENAMEASIDENTIFIER___Presenter: ___FILEBASENAMEASIDENTIFIER___InteractorOutput {
+
+}
