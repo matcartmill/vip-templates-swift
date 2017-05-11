9f484f9c616ad222fcb434989958503d12aacb64
Jody Schofield <jodyscho@gmail.com>
2017-05-11 07:19:41 -0400

    Trying to pick a better name for the hidden Rx subject property

--- vip-templates/VIP+Rx.xctemplate/___FILEBASENAME___Interactor.swift
+++ vip-templates/VIP+Rx.xctemplate/___FILEBASENAME___Interactor.swift
@@ -3,18 +3,18 @@ import RxSwift
 class ___FILEBASENAMEASIDENTIFIER___Interactor {
 
     // Todo: Replace...
-    fileprivate let eventModel = PublishSubject<___FILEBASENAMEASIDENTIFIER___InteractorResponse>()
+    fileprivate let modelSubject = PublishSubject<___FILEBASENAMEASIDENTIFIER___InteractorResponse>()
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___Interactor: ___FILEBASENAMEASIDENTIFIER___InteractorInput {
     
     func fetchModel() {
-        eventModel.onNext(___FILEBASENAMEASIDENTIFIER___InteractorResponse())
+        modelSubject.onNext(___FILEBASENAMEASIDENTIFIER___InteractorResponse())
     }
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___Interactor: ___FILEBASENAMEASIDENTIFIER___InteractorOutput {
     var model: Observable<___FILEBASENAMEASIDENTIFIER___InteractorResponse> {
-        return eventModel as Observable<___FILEBASENAMEASIDENTIFIER___InteractorResponse>
+        return modelSubject as Observable<___FILEBASENAMEASIDENTIFIER___InteractorResponse>
     }
 }
